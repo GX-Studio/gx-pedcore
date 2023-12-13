f4dbc1d4335b0b39eb35f48ef9f83efcdf0f88df
Nishi <james.renaud.75@gmail.com>
2023-12-13 16:32:02 +0100

    1-0

---
+++ fxmanifest.lua
@@ -0,0 +1,90 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Ped Core'
+studio 'GX Studio'
+developper 'Nishikoto'
+version '1.0'
+
+files {
+    'files/ladybug/peds.meta',
+    'files/chatnoir/peds.meta',
+    'files/bananeftn/peds.meta',
+    'files/ghost/peds.meta',
+    'files/ben10/peds.meta',
+    'files/caveira/peds.meta',
+    'files/bknuckle/peds.meta',
+    'files/pingu/peds.meta',
+    'files/steve/peds.meta',
+    'files/saitama/peds.meta',
+    'files/crackshot/peds.meta',
+    'files/deadpool/peds.meta',
+    'files/cuddlepool/peds.meta',
+    'files/onesis/peds.meta',
+    'files/veggiebao/peds.meta',
+    'files/nishi/peds.meta',
+    'files/todoroki/peds.meta',
+    'files/bakugo/peds.meta',
+    'files/demon/peds.meta',
+    'files/adminped/peds.meta',
+    'files/lavahead/peds.meta',
+    'files/juggernaut/peds.meta',
+    'files/venti/peds.meta',
+    'files/freedy/peds.meta',
+    'files/hulkvenom/peds.meta',
+    'files/tvman/peds.meta',
+    'files/hulknoel/peds.meta',
+    'files/purple/peds.meta',
+    'files/goldultron/peds.meta',
+    'files/whitetiger/peds.meta',
+    'files/jiahui/peds.meta',
+    'files/jiahuizombie/peds.meta',
+    'files/jedical/peds.meta',
+    'files/femmenu/peds.meta',
+    'files/mason/peds.meta',
+    'files/aether/peds.meta',
+    'files/freddy2/peds.meta',
+    'files/leatherface/peds.meta',
+    'files/goldberg/peds.meta',
+}
+    
+data_file 'PED_METADATA_FILE' 'files/ladybug/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/chatnoir/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/chatnoir/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/ghost/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/ben10/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/caveira/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/bknuckle/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/pingu/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/steve/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/saitama/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/crackshot/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/deadpool/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/cuddlepool/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/onesis/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/veggiebao/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/nishi/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/todoroki/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/bakugo/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/demon/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/bananeftn/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/adminped/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/lavahead/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/juggernaut/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/venti/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/freedy/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/hulkvenom/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/tvman/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/hulknoel/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/purple/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/goldultron/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/whitetiger/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/jiahui/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/jiahuizombie/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/jedical/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/femmenu/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/mason/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/aether/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/freddy2/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/leatherface/peds.meta'
+data_file 'PED_METADATA_FILE' 'files/goldberg/peds.meta'
---
+++ readme.md
@@ -0,0 +1,44 @@
+# GX Ped Core
+
+`Version 1.0`
+
+Name of spawn
+
+- adminped
+- aether
+- bakugo
+- bananeftn
+- ben10
+- bknuckle
+- caveira
+- chatnoir
+- crackshot
+- cuddlepool
+- demon
+- femmenu
+- freddy2
+- freddy
+- ghost
+- goldberg
+- goldultron
+- hulknoel
+- hulkvenom
+- jedical
+- jiahui
+- jiahuizombie
+- juggernaut
+- ladybug
+- lavahead
+- leatherfce
+- mason
+- nishi (Izuku Midoriya)
+- onesis
+- pingu
+- purple
+- saitama (minecraft)
+- steve (minecraft)
+- todoroki
+- tvman
+- veggiebao
+- venti
+- whitetiger
